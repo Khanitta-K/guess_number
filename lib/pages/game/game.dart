c5009ddf9d03392a2ee3c9dc086c48153c48535e
Khanitta Khongsanthia <khongsanthia_k@silpakorn.edu>
2023-01-14 16:38:59 +0700

    add function

---
+++ lib/pages/game/game.dart
@@ -0,0 +1,32 @@
+import 'dart:math';
+
+// enumeration
+enum GuessResult { correct, tooHigh, tooLow }
+
+class Game {
+  final int answer;
+  int _totalGuesses = 0;
+
+  // constructor
+  Game({required int maxRandom}) :
+        answer = Random().nextInt(maxRandom) + 1 {
+    print('Answer is $answer');
+  }
+
+  GuessResult doGuess(int guess) {
+    _totalGuesses++;
+
+    if (guess == answer) {
+      return GuessResult.correct;
+    } else if (guess > answer) {
+      return GuessResult.tooHigh;
+    } else {
+      return GuessResult.tooLow;
+    }
+  }
+
+  // getter
+  int get totalGuesses {
+    return _totalGuesses;
+  }
+}
--- lib/pages/game/game_page.dart
+++ lib/pages/game/game_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:guess_number/pages/game/game.dart';
 
 class GamePage extends StatefulWidget {
   const GamePage({Key? key}) : super(key: key);
@@ -8,12 +9,25 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
-  var space = 10.00;
-
+  final _controller = TextEditingController();
+  final _game = Game(maxRandom: 100);
+  var _feedBackText = '';
+  var _boxCount = 0;
+  var _showBox = true;
   @override
   Widget build(BuildContext context) {
+    var brownBox = Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(width: 20, height: 20, color: Colors.brown),
+    );
+    var orangeBox = Container(width: 20, height: 20, color: Colors.orange);
+    var boxList = <Widget>[];
+    for(var i = 0; i< _boxCount ; i++){
+      boxList.add(brownBox);
+    }
+
     return Scaffold(
-      appBar : AppBar(
+      appBar: AppBar(
         title: Row(
           children: [
             Container(
@@ -26,22 +40,94 @@ class _GamePageState extends State<GamePage> {
                 color: Theme.of(context).primaryColor,
               ),
             ),
-            SizedBox(width: space),
-            Text('GUESS THE NUMBER')
+            SizedBox(width: 10),
+            Text('GUESS THE NUMBER'),
           ],
-        ) ,
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text('Please guess the number between 1 and 100'),
-          TextField(),
-          OutlinedButton(
-              onPressed: (){
-                //todo:
-              },
-              child: Text('guess')),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: _showBox? boxList : [] ,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 60.0),
+                    child: Text(
+                      'Please guess the number between 1 and 100',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Theme.of(context).primaryColor,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  TextField(
+                    controller: _controller,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Enter your guess',
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(20.0),
+                    child: OutlinedButton(
+                      onPressed: () {
+                        //todo:
+                        var input = _controller.text;
+                        var guess = int.tryParse(input);
+
+                        var result = _game.doGuess(guess!);
+
+                        setState(() {
+                          if(result == GuessResult.correct){
+                            _feedBackText = 'Correct!!';
+                          }else if(result == GuessResult.tooHigh){
+                            _feedBackText = 'TOO High, Please try again';
+                          }else{
+                            _feedBackText = 'TOO LOW, Please try again';
+                          }
+                        });
+
+                        print(result);
+                      },
+                      child: Text('GUESS'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: OutlinedButton(
+                        onPressed: (){
+                          setState(() {
+                            _boxCount++;
+                          });
+
+                        },
+                        child: Text('TEST')
+                    ),
+                  ),
+                  OutlinedButton(
+                      onPressed: (){
+                          setState(() {
+                            _showBox = !_showBox;
+                          });
+                      },
+                      child: Text('TOGGLE')
+                  ),
+                ],
+              ),
+            ),
+            Text(_feedBackText),
+          ],
+        ),
       ),
     );
   }
